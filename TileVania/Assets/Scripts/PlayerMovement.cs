016f3c7a8a6a9ba93157528c123685f452055703
86award <58709759+86award@users.noreply.github.com>
2024-08-14 17:39:47 +0100

    Completed half of lecture 15

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,61 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector2 move;
-    Rigidbody2D rb;
+    Vector2 moveInput;
+    bool jumpInput;
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
 
     [SerializeField] float runSpeed = 5;
+    [SerializeField] float jumpForce = 5;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
     }
-
+    
     private void Run()
     {
-        rb.linearVelocityX = move.x * runSpeed;
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.linearVelocity.y);
+        myRigidbody.linearVelocity = playerVelocity;
+
+        bool playerHasVelocity = Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasVelocity);
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasVelocity = Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon;
+        if (playerHasVelocity) transform.localScale = new Vector2 (MathF.Sign(myRigidbody.linearVelocity.x), 1f);
     }
 
     void OnMove(InputValue value)
     {
-        move = value.Get<Vector2>();
+        moveInput = value.Get<Vector2>();
     }
 
-    
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Debug.Log(myRigidbody.linearVelocity.x);
+            //myRigidbody.linearVelocity += new Vector2(0f, jumpForce); // this works
+            //myRigidbody.AddForce(new Vector2(0f, jumpForce)); // this works but requires much greater force i.e. 500
+            myRigidbody.linearVelocityY = jumpForce; // even at 500 this doesn't work
+            // It's because I was using x and not y 🙄
+            Debug.Log($"After pressing jump, .x is " + myRigidbody.linearVelocity.x);
+            Debug.Log($"After pressing jump, the vector is " + myRigidbody.linearVelocity);
+        }
+    }
 }
