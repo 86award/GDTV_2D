e1ccb753e2cf9f3fb3745c134a0f74d0c0c9b309
86award <58709759+86award@users.noreply.github.com>
2024-09-15 21:25:38 +0100

    Completed 5.19

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -9,29 +9,51 @@ public class PlayerMovement : MonoBehaviour
     bool jumpInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    float baseGravityValue;
 
-    [SerializeField] float runSpeed = 5;
-    [SerializeField] float jumpForce = 5;
+    [SerializeField] float runSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] float climbSpeed;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        baseGravityValue = myRigidbody.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        Climb();
     }
     
     private void Run()
     {
-        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.linearVelocity.y);
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.linearVelocityY);
         myRigidbody.linearVelocity = playerVelocity;
 
         bool playerHasVelocity = Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("isRunning", playerHasVelocity);
+    }
+
+    private void Climb()
+    {
+        if (GetComponent<Collider2D>().IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myRigidbody.gravityScale = 0f;
+            Vector2 climbVelocity = new Vector2(myRigidbody.linearVelocityX, moveInput.y * climbSpeed);
+            myRigidbody.linearVelocity = climbVelocity;
+
+            bool playerHasVelocity = Mathf.Abs(myRigidbody.linearVelocity.y) > Mathf.Epsilon;
+            myAnimator.SetBool("isClimbing", playerHasVelocity);
+        }
+        else
+        {
+            myRigidbody.gravityScale = baseGravityValue;
+            myAnimator.SetBool("isClimbing", false);
+        }
     }
 
     private void FlipSprite()
@@ -47,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        if (value.isPressed)
+        if (value.isPressed && GetComponent<Collider2D>().IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             Debug.Log(myRigidbody.linearVelocity.x);
             //myRigidbody.linearVelocity += new Vector2(0f, jumpForce); // this works
